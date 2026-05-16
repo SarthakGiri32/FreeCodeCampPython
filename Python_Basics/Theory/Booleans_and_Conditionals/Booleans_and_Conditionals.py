c3a0d70a2f490a7253b9f995e24110b2cc0df88f
Sarthak Giri <giri.sarthak1@gmail.com>
2026-05-16 21:23:08 +0530

    Rearranging all theory and workshop files inside "Python_Basics" folder

---
+++ Python_Basics/Theory/Booleans_and_Conditionals/Booleans_and_Conditionals.py
@@ -0,0 +1,9 @@
+is_citizen = True
+age = 25
+# age = 16
+
+if is_citizen:
+    if age >= 18:
+        print('You are eligible to vote') # You are eligible to vote
+else:
+    print('You are not eligible to vote')
