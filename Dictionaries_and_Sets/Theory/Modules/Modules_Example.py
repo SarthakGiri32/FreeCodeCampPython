60e75f9042ad4e19f0fd9ced7606df14870c6493
Sarthak Giri <giri.sarthak1@gmail.com>
2026-05-22 20:36:54 +0530

    Completed the theory section

---
+++ Dictionaries_and_Sets/Theory/Modules/Modules_Example.py
@@ -0,0 +1,10 @@
+from math import radians, sin, cos
+
+angle_degrees = 40
+angle_radians = radians(angle_degrees)
+
+sine_value = sin(angle_radians)
+cos_value = cos(angle_radians)
+
+print(sine_value) # 0.6427876096865393
+print(cos_value)  # 0.766044443118978
