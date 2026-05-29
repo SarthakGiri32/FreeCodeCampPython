f01f6e5f6142b2583b855330bba406b5b9af62a2
Sarthak Giri <giri.sarthak1@gmail.com>
2026-05-29 22:03:36 +0530

    Completed the name mangling section in OOP

---
+++ Learning_Sections/6_Object_Oriented_Programming/Theory/Inheritance_and_Polymorphism/Name_Mangling/Example_3.py
@@ -0,0 +1,11 @@
+class Parent:
+    def __init__(self):
+        self.__data = 'Parent data'
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__()
+        self.__data = 'Child data'
+
+c = Child()
+print(c.__dict__) # {'_Parent__data': 'Parent data', '_Child__data': 'Child data'}
