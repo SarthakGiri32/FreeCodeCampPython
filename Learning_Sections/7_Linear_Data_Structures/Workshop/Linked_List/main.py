96aa05036cb430b89504f187019cbb9d54dfe155
Sarthak Giri <giri.sarthak1@gmail.com>
2026-06-02 14:37:57 +0530

    Completed the "Build a Linked List" workshop

--- Learning_Sections/7_Linear_Data_Structures/Workshop/Linked_List/main.py
+++ Learning_Sections/7_Linear_Data_Structures/Workshop/Linked_List/main.py
@@ -22,15 +22,47 @@ class LinkedList:
             current_node.next = node
         self.length += 1
 
+    def remove(self, element):
+        previous_node = None
+        current_node = self.head
+        while current_node is not None and current_node.element != element:
+            previous_node = current_node
+            current_node = current_node.next
+        if current_node is None:
+            return
+        elif previous_node is not None:
+            previous_node.next = current_node.next
+        else:
+            self.head = current_node.next
+        self.length -= 1
+
+    def __str__(self):
+        linked_list_str = ""
+        current_node = self.head
+        i = 1
+        while current_node is not None:
+            linked_list_str += f"{i}. {current_node.element}\n"
+            current_node = current_node.next
+            i += 1
+        return linked_list_str.rstrip("\n")
+
 
 def main():
     my_list = LinkedList()
-    print(my_list.is_empty())
+    print(f"my_list.is_empty(): {my_list.is_empty()}")
 
+    print("\nAdding elements...")
     my_list.add(1)
     my_list.add(2)
-    print(my_list.is_empty())
-    print(my_list.length)
+    my_list.add(3)
+    print(f"my_list.is_empty(): {my_list.is_empty()}")
+    print(f"my_list.length: {my_list.length}")
+    print(f"my_list:\n{my_list}")
+
+    print("\nRemoving one element...")
+    my_list.remove(2)
+    print(f"my_list.length: {my_list.length}")
+    print(f"my_list:\n{my_list}")
 
 if __name__ == "__main__":
     main()
